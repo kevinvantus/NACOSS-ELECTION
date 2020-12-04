d89337db643abbc0865f3b7b155055185c5effa1
kevinvantus <kevinvantus@gmail.com>
2020-12-04 16:29:42 +0100

    Rebuilt login page

--- Nacoss-Election/script.js
+++ Nacoss-Election/script.js
@@ -1,41 +1,51 @@
-const matricNumber = document.querySelector("#matric-number");
+const matricNumber = document.querySelector("#matric");
 const password = document.querySelector("#password");
 const matricNumberError = document.querySelector(
   ".matric-number-error-message"
 );
 const passwordError = document.querySelector(".password-error-message");
 
-document.querySelector(".btn-submit").addEventListener("click", (e) => {
+const btnSubmit = document.querySelector(".btn-submit");
+
+matricNumber.addEventListener("keypress", (e) => {
+  if (matricNumber.value.length > 8) {
+    e.preventDefault();
+  }
+});
+
+password.addEventListener("keypress", (e) => {
+  if (password.value.length > 9) {
+    e.preventDefault();
+  }
+});
+
+btnSubmit.addEventListener("click", (e) => {
   // matric number validation
   if (!matricNumber.value.length) {
     // EMPTY CASE
     matricNumberError.innerText = "matric number is required";
     matricNumberError.classList.add("error-message");
+    e.preventDefault();
   } else if (matricNumber.value.length < 9) {
     // LESS THAN 9 CHARACTERS
     matricNumberError.innerText =
       "matric number must not be less than 9 characters";
     matricNumberError.classList.add("error-message");
-  } else if (matricNumber.value.length > 9) {
-    // MORE THAN 9 CHARACTERS
-    matricNumberError.innerText =
-      "matric number must not be more than 9 characters";
-    matricNumberError.classList.add("error-message");
+    e.preventDefault();
   } else {
     matricNumberError.innerText = "";
   }
 
   // password validation
   if (!password.value.length) {
-    passwordError.textContent = "password is required";
+    passwordError.innerText = "password is required";
     passwordError.classList.add("error-message");
+    e.preventDefault();
   } else if (password.value.length < 10) {
-    passwordError.textContent = "password must not be less than 10 characters";
+    passwordError.innerText = "password must not be less than 10 characters";
     passwordError.classList.add("error-message");
-  } else if (password.value.length > 10) {
-    passwordError.textContent = "password must not be more than 10 characters";
-    passwordError.classList.add("error-message");
+    e.preventDefault();
   } else {
-    passwordError.textContent = "";
+    passwordError.innerText = "";
   }
 });
